cc34b3ab476cc4431f5503b65d874c4d62b8c391
Rupert Smith <rupert@thesett.com>
2019-03-19 14:03:15 +0000

    Functions for building and combining indexes.

--- src/Index/Index.elm
+++ src/Index/Index.elm
@@ -1,4 +1,4 @@
-module Index.Index exposing (Index, empty)
+module Index.Index exposing (Index, empty, fromString, fromStrings, union)
 
 import Set exposing (Set)
 
@@ -7,5 +7,27 @@ type Index
     = Index (Set String)
 
 
+empty : Index
 empty =
     Index Set.empty
+
+
+fromString : String -> Index
+fromString val =
+    String.words val
+        |> Set.fromList
+        |> Index
+
+
+fromStrings : List String -> Index
+fromStrings vals =
+    List.map String.words vals
+        |> List.concat
+        |> Set.fromList
+        |> Index
+
+
+union : Index -> Index -> Index
+union (Index set1) (Index set2) =
+    Set.union set1 set2
+        |> Index
