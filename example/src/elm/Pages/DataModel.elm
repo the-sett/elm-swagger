fc2969c3d46f3bdc40417f5de3e1c3b7bd8a90a5
Rupert Smith <rupert@thesett.com>
2019-04-03 15:43:00 +0100

    Implemented a simple search on schema names.

--- example/src/elm/Pages/DataModel.elm
+++ example/src/elm/Pages/DataModel.elm
@@ -20,7 +20,7 @@ view =
             []
             [ case model.state of
                 Loaded api ->
-                    dataModelView api
+                    dataModelView model.searchTerm api
 
                 _ ->
                     div [] []
@@ -81,39 +81,52 @@ uncurry fn =
     \( fst, snd ) -> fn fst snd
 
 
-dataModelView : OpenApi.OpenApi -> Html.Styled.Html Msg
-dataModelView spec =
+dataModelView : Maybe String -> OpenApi.OpenApi -> Html.Styled.Html Msg
+dataModelView maybeSearch spec =
     div
         []
         [ case spec.components of
             Just components ->
-                componentsView components
+                componentsView maybeSearch components
 
             Nothing ->
                 text "No schemas"
         ]
 
 
-componentsView : OpenApi.Components -> Html.Styled.Html Msg
-componentsView components =
+componentsView : Maybe String -> OpenApi.Components -> Html.Styled.Html Msg
+componentsView maybeSearch components =
     div
         []
-        (List.map (uncurry schemaView) (Dict.toList components.schemas))
+        (List.map (uncurry (schemaView maybeSearch)) (Dict.toList components.schemas))
 
 
-schemaView : String -> Json.Schema.Definitions.Schema -> Html.Styled.Html Msg
-schemaView name schema =
-    div []
-        [ text name
-        , styled div
-            [ Css.paddingLeft <| Css.px 10
-            , Css.fontSize <| Css.px 14
-            ]
-            []
-            [ pre []
-                [ Json.Schema.Definitions.encode schema
-                    |> Json.Encode.encode 4
-                    |> text
+schemaView : Maybe String -> String -> Json.Schema.Definitions.Schema -> Html.Styled.Html Msg
+schemaView maybeSearch name schema =
+    let
+        matches =
+            case maybeSearch of
+                Nothing ->
+                    True
+
+                Just term ->
+                    String.contains (String.toLower term) (String.toLower name)
+    in
+    if matches then
+        div []
+            [ text name
+            , styled div
+                [ Css.paddingLeft <| Css.px 10
+                , Css.fontSize <| Css.px 14
+                ]
+                []
+                [ pre []
+                    [ Json.Schema.Definitions.encode schema
+                        |> Json.Encode.encode 4
+                        |> text
+                    ]
                 ]
             ]
-        ]
+
+    else
+        div [] []
