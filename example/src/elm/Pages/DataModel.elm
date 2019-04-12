dc7f799b6c1d97b5fbb6ebc4f72cd30f46cf6f42
Rupert Smith <rupert@thesett.com>
2019-04-12 11:20:02 +0100

    Applied the same look and feel to the data model page.

--- example/src/elm/Pages/DataModel.elm
+++ example/src/elm/Pages/DataModel.elm
@@ -1,5 +1,6 @@
 module Pages.DataModel exposing (view)
 
+import Colors
 import Css
 import Dict
 import Html.Styled exposing (div, h1, h2, h3, h4, li, ol, p, pre, styled, text, ul)
@@ -9,6 +10,7 @@ import Json.Encode
 import Json.Schema.Definitions
 import Maybe.Extra
 import OpenApi.Model as OpenApi
+import Responsive exposing (ResponsiveStyle)
 import State exposing (Model, Msg(..), ViewState(..))
 import Structure exposing (Template(..))
 import ViewUtils exposing (highlight, optionalFlagField, optionalTextField, uncurry)
@@ -21,7 +23,7 @@ view =
             []
             [ case model.state of
                 Loaded api ->
-                    dataModelView model.searchTerm api
+                    dataModelView devices model.searchTerm api
 
                 _ ->
                     div [] []
@@ -35,28 +37,28 @@ view =
 -- View for the OpenAPI Spec
 
 
-dataModelView : Maybe String -> OpenApi.OpenApi -> Html.Styled.Html Msg
-dataModelView maybeSearch spec =
+dataModelView : ResponsiveStyle -> Maybe String -> OpenApi.OpenApi -> Html.Styled.Html Msg
+dataModelView devices maybeSearch spec =
     div
         []
         [ case spec.components of
             Just components ->
-                componentsView maybeSearch components
+                componentsView devices maybeSearch components
 
             Nothing ->
                 text "No schemas"
         ]
 
 
-componentsView : Maybe String -> OpenApi.Components -> Html.Styled.Html Msg
-componentsView maybeSearch components =
+componentsView : ResponsiveStyle -> Maybe String -> OpenApi.Components -> Html.Styled.Html Msg
+componentsView devices maybeSearch components =
     div
         []
-        (List.map (uncurry (schemaView maybeSearch)) (Dict.toList components.schemas))
+        (List.map (uncurry (schemaView devices maybeSearch)) (Dict.toList components.schemas))
 
 
-schemaView : Maybe String -> String -> OpenApi.Schema -> Html.Styled.Html Msg
-schemaView maybeSearch name schema =
+schemaView : ResponsiveStyle -> Maybe String -> String -> OpenApi.Schema -> Html.Styled.Html Msg
+schemaView devices maybeSearch name schema =
     let
         matches =
             case maybeSearch of
@@ -72,7 +74,19 @@ schemaView maybeSearch name schema =
                 div [] [ text "$ref" ]
 
             OpenApi.SchemaInline inlineSchema ->
-                div []
+                styled div
+                    [ Responsive.deviceStyles devices
+                        (\device ->
+                            [ Css.marginBottom <| Css.px (Responsive.rhythm 0.5 device)
+                            , Css.marginTop <| Css.px (Responsive.rhythm 0.5 device)
+                            , Css.padding <| Css.px (Responsive.rhythm 0.5 device)
+                            , Css.border3 (Css.px 1) Css.solid Colors.midGrey
+                            ]
+                        )
+                    , Css.borderRadius (Css.px 4)
+                    , Css.backgroundColor Colors.paperWhite
+                    ]
+                    []
                     [ highlight maybeSearch name
                     , styled div
                         [ Css.paddingLeft <| Css.px 10
